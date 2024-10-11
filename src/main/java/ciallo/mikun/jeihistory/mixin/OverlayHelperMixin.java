51e168571535e427290abc12cd213dfe776473d1
ccPori <empty_lamp@proton.me>
2024-10-12 02:41:17 +0800

    fix something i dont know

--- src/main/java/ciallo/mikun/jeihistory/mixin/OverlayHelperMixin.java
+++ src/main/java/ciallo/mikun/jeihistory/mixin/OverlayHelperMixin.java
@@ -9,25 +9,23 @@ import mezz.jei.common.config.IClientConfig;
 import mezz.jei.common.config.IClientToggleState;
 import mezz.jei.common.config.IIngredientFilterConfig;
 import mezz.jei.common.config.IIngredientGridConfig;
-import mezz.jei.common.gui.elements.DrawableNineSliceTexture;
+import mezz.jei.common.gui.textures.Textures;
 import mezz.jei.common.input.IInternalKeyMappings;
 import mezz.jei.common.network.IConnectionToServer;
+import mezz.jei.gui.filter.IFilterTextSource;
 import mezz.jei.gui.overlay.IIngredientGridSource;
 import mezz.jei.gui.overlay.IngredientGridWithNavigation;
+import mezz.jei.gui.overlay.IngredientListOverlay;
 import mezz.jei.gui.startup.OverlayHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-/**
- * @author mikun_12138
- * @date 2024/10/9 下午1:25
- */
 @Mixin(value = OverlayHelper.class, remap = false)
 public class OverlayHelperMixin {
-    @Inject(method = "createIngredientGridWithNavigation", at = @At("RETURN"), cancellable = true)
-    private static void IngredientGridWithNavigation(String debugName, IIngredientGridSource ingredientFilter, IIngredientManager ingredientManager, IIngredientGridConfig ingredientGridConfig, DrawableNineSliceTexture background, DrawableNineSliceTexture slotBackground, IInternalKeyMappings keyMappings, IIngredientFilterConfig ingredientFilterConfig, IClientConfig clientConfig, IClientToggleState toggleState, IConnectionToServer serverConnection, IColorHelper colorHelper, IScreenHelper screenHelper, boolean supportsEditMode, CallbackInfoReturnable<IngredientGridWithNavigation> cir) {
+    @Inject(method = "createIngredientListOverlay", at = @At("HEAD"), cancellable = true)
+    private static void IngredientGridWithNavigation(IIngredientManager ingredientManager, IScreenHelper screenHelper, IIngredientGridSource ingredientFilter, IFilterTextSource filterTextSource, IInternalKeyMappings keyMappings, IIngredientGridConfig ingredientGridConfig, IClientConfig clientConfig, IClientToggleState toggleState, IConnectionToServer serverConnection, IIngredientFilterConfig ingredientFilterConfig, Textures textures, IColorHelper colorHelper, CallbackInfoReturnable<IngredientListOverlay> cir) {
         JeiHistoryPlugin.historyIngredientListGrid = new HistoryIngredientListGrid(
                 ingredientManager,
                 ingredientGridConfig,
@@ -37,23 +35,33 @@ public class OverlayHelperMixin {
                 serverConnection,
                 keyMappings,
                 colorHelper,
-                supportsEditMode
+                true
+        );
+
+        IngredientGridWithNavigation ingredientGridWithNavigation1 = new IngredientGridWithNavigation(
+                "IngredientListOverlay",
+                ingredientFilter,
+                // here
+                JeiHistoryPlugin.historyIngredientListGrid,
+                toggleState,
+                clientConfig,
+                serverConnection,
+                ingredientGridConfig,
+                textures.getIngredientListBackground(),
+                textures.getIngredientListSlotBackground(),
+                screenHelper,
+                ingredientManager
         );
 
         cir.setReturnValue(
-                new IngredientGridWithNavigation(
-                        debugName,
+                new IngredientListOverlay(
                         ingredientFilter,
-                        // here
-                        JeiHistoryPlugin.historyIngredientListGrid,
-                        toggleState,
-                        clientConfig,
-                        serverConnection,
-                        ingredientGridConfig,
-                        background,
-                        slotBackground,
+                        filterTextSource,
                         screenHelper,
-                        ingredientManager
+                        ingredientGridWithNavigation1,
+                        clientConfig,
+                        toggleState,
+                        keyMappings
                 )
         );
     }
